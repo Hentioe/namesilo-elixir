e3165400f02a2d8705e81317641dc8d70d5d814e
Hentioe <me@bluerain.io>
2021-02-03 02:25:43 +0800

    Add deps

--- mix.exs
+++ mix.exs
@@ -23,7 +23,13 @@ defmodule NameSilo.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.23.0", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:elixir_xml_to_map, "~> 2.0"},
+      {:httpoison, "~> 1.8"},
+      {:typed_struct, "~> 0.2.1"}
     ]
   end
 end
